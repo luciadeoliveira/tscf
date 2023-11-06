4c4686945cd786fe963129314f18dd21e8bb2404
luciadeoliveira <lucia.de.oliveira@fing.edu.uy>
2023-11-06 14:57:42 -0300

    python

---
+++ paquete.py
@@ -0,0 +1,31 @@
+from scapy.all import Ether, IP, UDP, sendp
+
+# Configura las direcciones MAC e IP
+src_mac = "28:80:23:a9:b4:7c"
+dst_mac = "00:15:4d:13:5C:AC"
+src_ip = "192.168.60.241"
+dst_ip = "10.2.0.1"
+
+# Configura los puertos de origen y destino
+src_port = "eno1"
+dst_port = "vf0_1"
+
+# Crea el contenido del mensaje UDP
+payload = "Este es el mensaje que quiero enviar"
+
+# Construye el paquete Ethernet con las direcciones MAC
+eth = Ether(src=src_mac, dst=dst_mac)
+
+# Construye el paquete IP con las direcciones IP
+ip = IP(src=src_ip, dst=dst_ip)
+
+# Construye el segmento UDP con los puertos y el mensaje
+udp = UDP(sport=src_port, dport=dst_port)
+
+# Construye el paquete final
+packet = eth / ip / udp / payload
+
+# Envía el paquete
+sendp(packet, iface="tu_interfaz_de_red")
+
+print("Paquete enviado.")
