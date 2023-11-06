770844ea509ee77bb5ec892122f51d70cc0e6264
luciadeoliveira <lucia.de.oliveira@fing.edu.uy>
2023-11-06 16:53:18 -0300

    a

--- paquete.py
+++ paquete.py
@@ -2,13 +2,13 @@ from scapy.all import Ether, IP, UDP, sendp
 
 # Configura las direcciones MAC e IP
 src_mac = "28:80:23:a9:b4:7c"
-dst_mac = "00:15:4d:13:5C:AB"
+dst_mac = "00:15:4d:13:5C:AC"
 src_ip = "192.168.60.241"
 dst_ip = "10.2.0.1"
 
 # Configura los puertos de origen y destino
 src_port = 12345    
-dst_port = 6890
+dst_port = 6789
 
 # Crea el contenido del mensaje UDP
 payload = "Este es el mensaje que quiero enviar"
